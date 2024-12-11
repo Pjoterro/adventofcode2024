b566763c83e8053cec58d87d798e3c99bcdec9a9
ubuntu <piotr00twardowski@gmail.com>
2024-12-11 09:08:39 +0100

    adding edge cases

--- day2.py
+++ day2.py
@@ -8,51 +8,70 @@ test_input = """7 6 4 2 1
 9 7 6 2 1
 1 3 2 4 5
 8 6 4 4 1
-1 3 6 7 9"""
+1 3 6 7 9
+48 46 47 49 51 54 56
+1 1 2 3 4 5
+1 2 3 4 5 5
+5 1 2 3 4 5
+1 4 3 2 1
+1 6 7 8 9
+1 2 3 4 3
+9 8 7 6 7
+7 10 8 10 11
+29 28 27 25 26 25 22 20"""
 
 test_safe = 2
-test_safe2 = 4
+test_safe2 = 14
 ### End of test case ###
 
 def filter_safe_results(all_results):
     results = []
     for line in all_results.splitlines():
-        if is_record_safe(line):
+        if (line == ""):
+            continue
+        if is_record_safe(line.split()):
             results.append(line)
     return results
 
-def is_record_safe(input):
-    record = input.split()
-    ascending_modifier = 1
-    if int(record[0]) > int(record[1]):
-        ascending_modifier = -1
-    for i in range(len(record)-1):
-        delta = (int(record[i+1]) - int(record[i])) * ascending_modifier
-        if delta < 1 or delta > 3:
-            return False
-    return True
-
-def is_record_safe2(input):
-    record = input.split()
-    ascending_modifier = 1
+def is_record_safe(array):
     recheck_flag = False
     recheck_index = 0
-    if int(record[0]) > int(record[-1]):
+    ascending_modifier = 1
+    if int(array[0]) > int(array[-1]):
         ascending_modifier = -1
-    for i in range(len(record) - 1):
-        delta = (int(record[i+1]) - int(record[i])) * ascending_modifier
+    for i in range(len(array) -1):
+        delta = (int(array[i+1]) - int(array[i])) * ascending_modifier
         if (delta < 1 or delta > 3):
             if recheck_flag:
+                print("----TO IGNORE\nindex: " + str(i) + "\n" + str(array))
                 return False
-            recheck_flag = True
-            recheck_index = i
+            else:
+                recheck_flag = True
+                recheck_index = i
     if not recheck_flag:
         return True
-    rerecord_1 = input.split()
-    rerecord_1.pop(recheck_index)
-    rerecord_2 = input.split()
-    rerecord_2.pop(recheck_index + 1)
-    flag_1 = 
+    print("----TO RECHECK\n" + str(array))
+    sub_array_1 = array.copy()
+    sub_array_2 = array.copy()
+    sub_array_1.pop(recheck_index)
+    flag_1 = True
+    sub_array_2 = array
+    sub_array_2.pop(recheck_index + 1)
+    flag_2 = True
+    for i in range(len(sub_array_1) - 1):
+        delta = (int(sub_array_1[i+1]) - int(sub_array_1[i])) * ascending_modifier
+        if (delta < 1 or delta > 3):
+            flag_1 = False
+            break
+    for i in range(len(sub_array_2) - 1):
+        delta = (int(sub_array_2[i+1]) - int(sub_array_2[i])) * ascending_modifier
+        if (delta < 1 or delta > 3):
+            flag_2 = False
+            break
+    print("index: " + str(recheck_index) + " | bools: " + str(flag_1 or flag_2))
+    return (flag_1 or flag_2)
+
+
 ### main: ###
 if mode == "TEST":
     input = test_input
@@ -63,4 +82,4 @@ elif mode == "TASK":
 buffer = filter_safe_results(input)
 print("no. of safe reports: " + str(len(buffer)))
 if mode == "TEST":
-    print("test status: " + str(len(buffer) == test_safe))
+    print("test status: " + str(len(buffer) == test_safe2))
